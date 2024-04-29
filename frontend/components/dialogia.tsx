ae5fb070d8f1f02238b898f46e1f9700c339bf82
trojrobert <trojrobert@gmail.com>
2024-04-29 17:13:19 +0200

    format frontend

--- frontend/components/dialogia.tsx
+++ frontend/components/dialogia.tsx
@@ -1,6 +1,5 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-import './LandingPage.css'; 
+import React from 'react';
+import './LandingPage.css';
 
 const LandingPage: React.FC = () => {
   return (
@@ -20,25 +19,6 @@ const LandingPage: React.FC = () => {
         </p>
         
         <p className="coming-soon">Coming Soon</p>
-        {/* <p>
-          Whether you're a beginner or an advanced learner, Dialogia offers a variety of dialogues
-          tailored to your proficiency level. From everyday conversations to specific situational
-          dialogues, you'll have access to a rich library of content that will enhance your
-          language learning journey.
-        </p>
-        <h2>Key Features:</h2>
-        <ul>
-          <li>Engaging dialogues for immersive learning</li>
-          <li>Contextual understanding of words and phrases</li>
-          <li>Progress tracking to monitor your language skills</li>
-          <li>Customizable learning paths based on your goals</li>
-          <li>Available in multiple languages for diverse learners</li>
-        </ul>
-        <p>
-          Join us on our mission to make language learning more enjoyable and effective. Sign up
-          to receive updates and be notified when Dialogia launches!
-        </p> */}
-        {/* <Link to="/signup" className="cta-button">Sign Up for Updates</Link> */}
       </section>
       <footer>
         <p>© 2024 Dialogia. All rights reserved.</p>
