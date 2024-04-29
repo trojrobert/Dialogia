98e06d773139353cb21dbde33556e84aa9998c97
amahJennifer <jennifer000@rocketmail.com>
2024-04-29 18:56:56 +0200

    align landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import LandingPage from "@/components/dialogia";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="main-content flex min-h-screen flex-col items-center p-24 ">
 
       <head>
         <title>
