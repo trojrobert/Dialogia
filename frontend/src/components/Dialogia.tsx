03975518132d5668d4ac3154b97f4c1b4c6060d2
amahJennifer <jennifer000@rocketmail.com>
2024-05-01 19:12:45 +0200

    FIx import error

--- frontend/src/components/Dialogia.tsx
+++ frontend/src/components/Dialogia.tsx
@@ -1,9 +1,7 @@
 "use client"; // This is a client component 👈🏽
-
 import React, {useEffect, useState} from 'react';
-// import "./LandingPage.css";
 import DialogueForm from "./DialogueForm/DialogueForm";
-
+import './Styles/LandingPage.css';
 
 const LandingPage: React.FC = () => {
   
