064c6b418818cd00a99cbaf9a2546df325ab9e7d
trojrobert <trojrobert@gmail.com>
2024-05-04 07:15:27 +0200

    add translating language argument

--- backend/app/diagloue.py
+++ backend/app/diagloue.py
@@ -8,35 +8,37 @@ import argparse
 MAX_PROMPT_LENGTH = os.getenv("MAX_PROMPT_LENGTH")
 def main():
     
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--prompt", "-i", type=str, required=True)
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--prompt", "-i", type=str, required=True)
+    # args = parser.parse_args()
     
-    user_prompt = args.prompt
+    # user_prompt = args.prompt
     
-    if validate_prompt_lenght(user_prompt):
+    # if validate_prompt_lenght(user_prompt):
         
-        dialogue = generate_dialogue(user_prompt=user_prompt)
-        print(f"Dialogue - {dialogue}")
+    #     dialogue = generate_dialogue(user_prompt=user_prompt)
+    #     print(f"Dialogue - {dialogue}")
     
-    else: 
-        raise ValueError(f"""Input too long. Length of input = {len(user_prompt)}. Number of words must be less that {MAX_PROMPT_LENGTH}""")
-    
+    # else: 
+    #     raise ValueError(f"""Input too long. Length of input = {len(user_prompt)}. Number of words must be less that {MAX_PROMPT_LENGTH}""")
+    pass
 
-def generate_dialogue(user_prompt: str) -> str:
+def generate_dialogue(user_prompt: str,
+                      tranlating_language: str) -> str:
     client = OpenAI()
 
-    print(f"User prompt - {user_prompt}")
-    content = f"Translate the {user_prompt} to German and create a sentence using the translated word"
+    # print(f"User prompt - {user_prompt}")
+    content = f"Create a diagloue that to explain {user_prompt}. Then translate the diagloue to {tranlating_language}"
     print(f"System prompt - {content}")
     
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a language teacher, you translate words from english to german can create a sentence"},
+        {"role": "system", "content": """You are a language teacher, you teach languages by creating a dialogue based on a word or topic \
+            first you create a real life dialogue then translate this dialogue to another language."""},
         {"role": "user", "content": content}
     ], 
-    max_tokens=20,
+    max_tokens=50,
     )
 
     dialogue = completion.choices[0].message.content
