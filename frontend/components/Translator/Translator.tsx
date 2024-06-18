129579a8b09452e109cd06d2fc7a1cc6f9e5b284
trojrobert <trojrobert@gmail.com>
2024-06-18 13:31:54 +0200

    format the divs

--- frontend/components/Translator/Translator.tsx
+++ frontend/components/Translator/Translator.tsx
@@ -5,29 +5,30 @@ export default function Translator(){
     return (
         <div className={'translator-container'}>
             
-            <input  type="text" id="user-prompt" name="user-input"/>
-    
-            <div className={'translator-container-item'}>
-                <select name="origin-lang" id="lang-select">
-                    <option value="">Input Language</option>
-                    <option value="english">English</option>
-                </select>
-            </div>
-            {/* <div className={'translator-box'}>
-                <textarea id="origin-lang" name="origin-lang" />
-            </div> */}
-            
+            <div className="translator-row">
+                <input  type="text" id="user-prompt" name="user-input"/>
         
-            <div className={'translator-container-dropdown'}>
-                <select name="origin-lang" id="lang-select">
-                    <option value="">Output Language</option>
-                    <option value="german">German</option>
-                </select>
+                <div className={'translator-container-item'}>
+                    <select name="origin-lang" id="lang-select">
+                        <option value="">Input Language</option>
+                        <option value="english">English</option>
+                    </select>
+                </div>
+                {/* <div className={'translator-box'}>
+                    <textarea id="origin-lang" name="origin-lang" />
+                </div> */}
+                
+            
+                <div className={'translator-container-item'}>
+                    <select name="origin-lang" id="lang-select">
+                        <option value="">Output Language</option>
+                        <option value="german">German</option>
+                    </select>
+                </div>
+                {/* <div className={'translator-box'}>
+                    <textarea id="lang-text" name="lang-text"/>
+                </div> */}
             </div>
-            {/* <div className={'translator-box'}>
-                <textarea id="lang-text" name="lang-text"/>
-            </div> */}
-       
             <div className={'button-container'}>
                 <button className={'submit-button'} type='submit'>Create Dialogue</button>
             </div>
